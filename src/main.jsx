6208ac9b923db98b6e20561ad97530a16fd540e1
MichaelKehoe4318 <Michaelkehoecolab@gmail.com>
2025-10-21 11:56:09 +0100

    Added trending this week page

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader';
 import MoviesContextProvider from "./contexts/moviesContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage'
+import TrendingMoviesPage from './pages/trendingMoviesPage'
 
 
 
@@ -34,6 +35,7 @@ const App = () => {
             <Route path="/reviews/:id" element={<MovieReviewPage />} />
             <Route path="/reviews/form" element={<AddMovieReviewPage />} />
             <Route path="/movies/:id" element={<MoviePage />} />
+            <Route path="/movies/trending" element={<TrendingMoviesPage /> } />
             <Route path="/" element={<HomePage />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
